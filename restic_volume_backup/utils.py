ac33ee39dde73a4e3d7bf5976619726a0106d3c9
Einar Forselv <eforselv@gmail.com>
2019-04-16 18:22:03 +0200

    Create config in list_containers

--- restic_volume_backup/utils.py
+++ restic_volume_backup/utils.py
@@ -5,7 +5,8 @@ from restic_volume_backup.config import Config
 
 def list_containers():
     """Easily mockable container list"""
-    client = docker.Client(base_url=Config.docker_base_url)
+    config = Config()
+    client = docker.Client(base_url=config.docker_base_url)
     all_containers = client.containers()
     client.close()
     return all_containers
