9d3d87d9d53f36dd177e64aeaeb973d47310a55e
Einar Forselv <eforselv@gmail.com>
2019-04-16 18:21:42 +0200

    Make config check skippable for tests

--- restic_volume_backup/config.py
+++ restic_volume_backup/config.py
@@ -2,11 +2,13 @@ import os
 
 
 class Config:
-    def __init__(self):
+    def __init__(self, check=True):
         self.repository = os.environ['RESTIC_REPOSITORY']
         self.password = os.environ['RESTIC_PASSWORD']
         self.docker_base_url = os.environ.get('DOCKER_BASE_URL') or "unix://tmp/docker.sock"
-        self.check()
+    
+        if check:
+            self.check()
 
     def check(self):
         if not self.repository:
