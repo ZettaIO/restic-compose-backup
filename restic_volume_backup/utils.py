1e43c91d8548ceb2cad407c246b80f2acec0f6f6
Einar Forselv <eforselv@gmail.com>
2019-04-16 02:22:23 +0200

    Remove test code in utils

--- restic_volume_backup/utils.py
+++ restic_volume_backup/utils.py
@@ -1,12 +1,11 @@
 import docker
-import pprint
 
 from restic_volume_backup.config import Config
 
 
 def list_containers():
+    """Easily mockable container list"""
     client = docker.Client(base_url=Config.docker_base_url)
     all_containers = client.containers()
-    pprint.pprint(all_containers, indent=2)
     client.close()
     return all_containers
