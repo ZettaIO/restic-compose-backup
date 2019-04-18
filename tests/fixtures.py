f67e9968f5b06f76732c697ebdf8d698de9ec035
Einar Forselv <eforselv@gmail.com>
2019-04-18 04:26:08 +0200

    Trim container fixtures of unused info

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -34,7 +34,6 @@ def containers(project="default", containers=[]):
         {
             'Id': container.get('id', generate_sha256()),
             'Name': ''.join(random.choice(string.ascii_lowercase) for i in range(16)),
-            'ImageID': 'sha256:4d9a81206af7d65563b85d06be160dc90dc20ade94edcf544261f0e1db4472b3',
             'Config': {
                 'Image': 'restic-volume-backup_backup',
                 'Labels': {
@@ -45,25 +44,6 @@ def containers(project="default", containers=[]):
                 },
             },
             'Mounts': container.get('mounts', []),
-            'NetworkSettings': {
-                'Networks': {
-                    'restic-volume-backup_default': {
-                        'Aliases': None,
-                        'DriverOpts': None,
-                        'EndpointID': '96ac233ec96ae2318a15419c4d211bbc0d130fc33baaaba6dcd8c13328ba4b8f',
-                        'Gateway': '172.22.0.1',
-                        'GlobalIPv6Address': '',
-                        'GlobalIPv6PrefixLen': 0,
-                        'IPAMConfig': None,
-                        'IPAddress': '172.22.0.2',
-                        'IPPrefixLen': 16,
-                        'IPv6Gateway': '',
-                        'Links': None,
-                        'MacAddress': '02:42:ac:16:00:02',
-                        'NetworkID': '98886e14fddc2b3d31d15fbbfc3ea1b339626374ba2ff6c2dad406b1617ee09c',
-                    }
-                }
-            },
             'State': {
                 "Status": "running",
                 "Running": True,
