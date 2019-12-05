c6b9f2dc1ebe2edf5ea33b0730ed43ea77a3db7b
Einar Forselv <eforselv@gmail.com>
2019-12-05 02:04:36 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="restic-compose-backup",
     url="https://github.com/ZettaIO/restic-compose-backup",
-    version="0.2.0",
+    version="0.3.0",
     author="Einar Forselv",
     author_email="eforselv@gmail.com",
     packages=find_namespace_packages(include=['restic_compose_backup']),
