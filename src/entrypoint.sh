a49b48646ad8cf9b74b7842b72ea6019c8f8ded4
rcastropolizainformatica <rcastro@polizainformatica.com>
2021-03-23 10:59:08 +0100

    Adding update-ca-certificates

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,6 +3,9 @@
 # Dump all env vars so we can source them in cron jobs
 printenv | sed 's/^\(.*\)$/export \1/g' > /env.sh
 
+# Update CA certificates
+update-ca-certificates
+
 # Write crontab
 rcb crontab > crontab
 
