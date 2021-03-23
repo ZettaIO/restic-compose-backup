a95d1bf44fef9b2ad153fb6c89514a5aa6405c93
Rubén Castro Gutiérrez <rcastro@polizainformatica.com>
2021-03-23 11:17:30 +0000

    modified:   entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,9 +3,6 @@
 # Dump all env vars so we can source them in cron jobs
 printenv | sed 's/^\(.*\)$/export \1/g' > /env.sh
 
-# Update CA certificates
-update-ca-certificates
-
 # Write crontab
 rcb crontab > crontab
 
