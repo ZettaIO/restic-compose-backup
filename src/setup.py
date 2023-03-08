be3b3668bcd2b787d8d177ebb29450bf46295622
einarf <eforselv@gmail.com>
2023-03-08 23:09:05 +0100

    Bump version + bump reqs and base image

--- src/setup.py
+++ src/setup.py
@@ -3,12 +3,13 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="restic-compose-backup",
     url="https://github.com/ZettaIO/restic-compose-backup",
-    version="0.6.0",
+    version="1.0.0",
     author="Einar Forselv",
     author_email="eforselv@gmail.com",
     packages=find_namespace_packages(include=['restic_compose_backup']),
     install_requires=[
-        'docker==4.1.*',
+        # 'docker==4.1.*',
+        'docker~=6.0.0',
     ],
     entry_points={'console_scripts': [
         'restic-compose-backup = restic_compose_backup.cli:main',
