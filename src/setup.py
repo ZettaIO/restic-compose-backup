28dda6b09d02741f34e2bcf0ab3e47a0dede0281
dreadper <dreadper@gmail.com>
2023-10-28 14:54:20 +0200

    fix TypeError: request() got an unexpected keyword argument 'chunked' by upgrading pip package docker (from 4.1.* to 5.1.*)

--- src/setup.py
+++ src/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_namespace_packages
 setup(
     name="restic-compose-backup",
     url="https://github.com/ZettaIO/restic-compose-backup",
-    version="0.6.0",
+    version="0.6.3",
     author="Einar Forselv",
     author_email="eforselv@gmail.com",
     packages=find_namespace_packages(include=['restic_compose_backup']),
     install_requires=[
-        'docker==4.1.*',
+        'docker==6.1.*',
     ],
     entry_points={'console_scripts': [
         'restic-compose-backup = restic_compose_backup.cli:main',
